d6111490e5613b539003a4d13ae2d49c4398dd54
mxyllllllllll <919435335@qq.com>
2017-08-22 14:47:45 +0800

    requirejs  弹出层

---
+++ requirejs/弹出层/js/dialog1.js
@@ -0,0 +1,48 @@
+requirejs.config({
+
+    paths:{
+        jquery:'jquery-1.12.4.js'
+    }
+});
+
+define(["jquery"],function ($) {
+    return{
+        open: function (settings) {
+            var defaultSettings={
+                width:500,
+                height:400,
+                title:"弹出层",
+                content:""
+            };
+            $.extend(defaultSettings, settings);
+
+            var html='<div class="dialog-container">'
+                +'<div class="dialog-mask"></div>'
+                +'<div class="dialog-main">'
+                +'<div class="dialog-title">'
+                +'<div class="dialog-title-item">'+defaultSettings.title+'</div>'
+                +'<div class="dialog-title-close">[X]</div>'
+                +'</div>'
+                +'<div class="dialog-content"></div>'
+                +'</div>'
+                +'</div>';
+            $("body").append(html);
+
+            $(".dialog-main").css({
+                width: defaultSettings.width,
+                height: defaultSettings.height,
+                marginTop : -defaultSettings.height / 2,
+                marginLeft : -defaultSettings.width / 2
+            });
+            if (defaultSettings.content.indexOf(".html")!=-1) {
+                $(".dialog-content").load(defaultSettings.content);
+            }else {
+                $(".dialog-content").html(defaultSettings.content);
+            }
+            $(".dialog-title-close").on("click",function(){
+                (this).parents(".dialog-container").remove();
+            });
+        }
+    };
+
+});
