a2761774933c66d30f5362089c80cdefc8870b36
mxyllllllllll <919435335@qq.com>
2017-11-24 17:14:38 +0800

    vue-cil

---
+++ vue-cli/vue-project/src/components/common/CommonFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='footer'>
+      <ul>
+          <li>电影</li>
+          <li>音乐</li>
+          <li>书籍</li>
+          <li>图片</li>
+      </ul>
+  </div>
+</template>
+<script>
+    
+</script>
+<style>
+   .footer{
+       height    : 1rem;
+       background: rgb(33, 150, 243);
+       position  : fixed;
+       bottom    : 0;
+       width     : 100%;
+   }
+   .footer ul{
+       display: flex;
+   }
+   .footer li{
+       flex       : 1;
+       text-align : center;
+       color      : #fff;
+       line-height: 1rem;
+   }
+</style>
