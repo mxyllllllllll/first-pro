baaf3ecbcea41ac93ab63f7a9462edde64ce60ce
mxyllllllllll <919435335@qq.com>
2017-11-24 17:33:41 +0800

    vue-cil

---
+++ vue-cli/vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <img src="../assets/logo.png">
+    <h1>{{ aa }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      aa : "fafdsafds"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vue-cli/vue-project/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+require("./assets/js/rem.js");
+require("./assets/css/reset.css");
+global.API_PROXY = 'https://bird.ioliu.cn/v1/?url='
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
