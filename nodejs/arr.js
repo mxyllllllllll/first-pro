187dad79731ccada9fd1337596abd6cdeee569e4
mxyllllllllll <919435335@qq.com>
2018-01-21 11:19:54 +0800

    nodejs

---
+++ nodejs/arr.js
@@ -0,0 +1,6 @@
+var sort = require('./arrSort');
+
+var a = [1,5,2,11];
+
+console.log(sort.arrSort(a));
+sort.fn();
---
+++ nodejs/node.js
@@ -0,0 +1,4 @@
+/**
+ * Created by a123 on 2018/1/19.
+ */
+console.log('hello world miao xin yu');
